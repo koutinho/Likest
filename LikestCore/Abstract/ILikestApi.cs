84f5e59ad93a606f8300c21507ac3fd5a7318125
UNI-ENG\a.vasiliev <a.vasiliev@uni-eng.ru>
2018-08-20 11:57:16 +0300

    likest API library is added

---
+++ LikestCore/Abstract/ILikestApi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikestCore.Abstract
+{
+    /// <summary>
+    /// Интерфейс API сервиса Likest
+    /// </summary>
+    public interface ILikestApi
+    {
+        /// <summary>
+        /// Загрузка системы
+        /// </summary>
+        IObservable<(double current, double slowdown)> Load { get; }
+    }
+}
