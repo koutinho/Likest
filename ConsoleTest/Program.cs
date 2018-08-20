0458cec2ffc0bc66c15e601bd50ab32b242955c0
UNI-ENG\a.vasiliev <a.vasiliev@uni-eng.ru>
2018-08-20 13:55:04 +0300

    Console test project is added

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using LikestCore.Abstract;
+using LikestCore.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILikestApi api = new RestSharpLikestApi();
+
+            var loadObservable = api.Load;
+
+            var subscription = loadObservable.Subscribe((t) => Console.WriteLine($"Current: {t.current}. Slowdown: {t.slowdown}"));
+
+            Console.ReadLine();
+
+            subscription.Dispose();
+        }
+    }
+}
